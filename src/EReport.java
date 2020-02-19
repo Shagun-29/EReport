d1b778279cfd70729263b5e02bebb78c8fadca5e
Shagun <shagunsharma2929@gmail.com>
2020-02-18 21:26:11 -0500

    employee.dat file added to project and output to console

--- src/EReport.java
+++ src/EReport.java
@@ -1,9 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class EReport {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		String path = "./employees.dat";
+		FileInputStream fis = new FileInputStream(path);
+		BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+		String s = null;
+		while ((s = in.readLine())!=null)
+	    {
+			System.out.println(s);
+	    }
 	}
 
 }
