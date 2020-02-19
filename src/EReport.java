4f7afc94ca8a3f8f304d5cdea84299dca06041da
Shagun <shagunsharma2929@gmail.com>
2020-02-18 21:41:00 -0500

    input file parsed

--- src/EReport.java
+++ src/EReport.java
@@ -2,19 +2,43 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import Models.Employee;
 
 public class EReport {
+
+	private static BufferedReader in;
+	private static ArrayList<Employee> employees = new ArrayList<Employee>();
 
 	public static void main(String[] args) throws IOException {
 		
 		String path = "./employees.dat";
 		FileInputStream fis = new FileInputStream(path);
-		BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+		in = new BufferedReader(new InputStreamReader(fis));
 		String s = null;
 		while ((s = in.readLine())!=null)
 	    {
-			System.out.println(s);
+			if(s.trim().charAt(0) != '#') {
+				try {
+					String emp[] = s.split(",");
+					String empName[] = emp[1].trim().split(" ");
+					int id = Integer.parseInt(emp[0]);
+					String fname = empName[0];
+					String lname = empName[1];
+					Employee employee = new Employee(id, fname, lname);
+					employees.add(employee);
+				} catch(Exception e) {
+					System.out.println("Error in parsing employee data...");
+					e.printStackTrace();
+				}
+				
+			}
 	    }
+		
+		for(Employee emp : employees) {
+			System.out.println(emp.getID() + "," + emp.getFirstName() + " " + emp.getLastName());
+		}
 	}
 
 }
